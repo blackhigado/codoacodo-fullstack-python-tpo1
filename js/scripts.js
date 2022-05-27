1420ef360ac56f6ecf08fa9a9a203fbf21e902e2
blackhigado <abernal.dev@gmail.com>
2022-05-27 16:58:21 -0300

    Se realiza validacion de email en formulario

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 console.log('CODO A CODO - TPO1');
 
-const submitBtn = document.getElementById('submit');
+const submitBtn = document.getElementById('submit-button');
 
 const validate = (e) => {
         e.preventDefault();
-        alert('Probando');
-        return true; // Can submit the form data to the server
+
+        const emailAddress = document.getElementById('email');
+        const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+        if (emailAddress.value.trim() === '') {
+                alert('ERROR: Email cannot be blank');
+                emailAddress.focus();
+                return false;
+        }
+
+        if (!emailAddress.value.match(validRegex)) {
+                alert('ERROR: Invalid email address.');
+                emailAddress.focus();
+                return false;
+        }
+
+        return true;
 };
 
 submitBtn.addEventListener('click', validate);
